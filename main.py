a9ce72c23b46984f5c947d6603b90160d91d56e4
Samuel Caetano <sam.kaetano@gmail.com>
2023-11-11 21:33:06 -0300

    added etl component

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from dotenv import dotenv_values
 from kfp.v2.dsl import pipeline
 from kfp.v2.compiler import Compiler
+from kfp.components import ComponentStore
 from kfp.v2 import dsl
 
 config = dotenv_values('.env')
 
+# Load components
+component_store = ComponentStore(local_search_paths=["components"])
+etl_op = component_store.load_component("etl")
 
 # Define pipeline
 @pipeline(
@@ -14,11 +18,14 @@ def create_pipeline():
     """ Create the execution pipeline flow 
     """
     # Load file from gcs
-    dsl.importer(
+    data = dsl.importer(
         artifact_uri = config['URI'],
         artifact_class = dsl.Dataset,
         reimport = True,
     )
+
+    etl_op(input_1=data.outputs['artifact'])
+
 
 
 # Compile pipeline and build json package to deploy in Vertex
