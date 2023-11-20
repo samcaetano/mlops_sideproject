a37c2f4a22f67178b672f6c8dd0eda4f2f75ef00
Samuel Caetano <sam.kaetano@gmail.com>
2023-11-20 19:14:04 -0300

    added files for pushing docker file of the split_data step

---
+++ components/split_data/build_image.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+image_name=gcr.io/my-project-space-404513/split_data
+image_tag=latest
+full_image_name=${image_name}:${image_tag}
+
+cd "$(dirname "$0")"
+docker build -t "${full_image_name}" .
+docker login
+docker push "$full_image_name"
+
+# Output the strict image name, which contains the sha256 image digest
+docker inspect --format="{{index .RepoDigests 0}}" "${full_image_name}"
